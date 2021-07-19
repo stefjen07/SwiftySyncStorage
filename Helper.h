861d085520fb66d6dec3c635097d297b878094de
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-20 01:21:00 +0300

    UUID size definition

--- Helper.h
+++ Helper.h
@@ -1,6 +1,8 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#define UUID_SIZE 36
+
 string create_uuid() {
     const char* v = "0123456789abcdef";
     const bool dash[] = { 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 };
