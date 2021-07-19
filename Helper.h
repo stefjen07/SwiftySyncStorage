d3ab99b38aa691ca7acff5d273a1bd481d343cb8
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-20 00:20:40 +0300

    Helper merge

---
+++ Helper.h
@@ -0,0 +1,17 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+string create_uuid() {
+    const char* v = "0123456789abcdef";
+    const bool dash[] = { 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 };
+
+    string res;
+    for (int i = 0; i < 16; i++) {
+        if (dash[i]) res += "-";
+        res += v[rand() % 16];
+        res += v[rand() % 16];
+    }
+    return res;
+}
+
+#endif
