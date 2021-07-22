559b358feb6b713c81c67dacb98aac7973e967ab
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-22 14:08:29 +0300

    DataUnit bugfix

--- Data.h
+++ Data.h
@@ -16,14 +16,22 @@ public:
 	void encode(CoderContainer* container) {
 		if (container->type == CoderType::json) {
 			JSONEncodeContainer* jsonContainer = dynamic_cast<JSONEncodeContainer*>(container);
-			jsonContainer->encode(bytes, "bytes");
+			string bytesStr;
+			for (int i = 0; i < bytes.size(); i++) {
+				bytesStr += bytes[i];
+			}
+			jsonContainer->encode(bytesStr, "bytes");
 		}
 	}
 
 	void decode(CoderContainer* container) {
 		if (container->type == CoderType::json) {
 			JSONDecodeContainer* jsonContainer = dynamic_cast<JSONDecodeContainer*>(container);
-			bytes = jsonContainer->decode(vector<dataByte>(), "bytes");
+			string bytesStr = jsonContainer->decode(string(), "bytes");
+			bytes = vector<dataByte>();
+			for (int i = 0; i < bytesStr.length(); i++) {
+				bytes.push_back(bytesStr[i]);
+			}
 		}
 	}
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <SwiftySyncStorage.h>
+#include <Request.h>
+#include <Data.h>
 #include <iostream>
 using namespace std;
 
 int main() {
+	FunctionRequest request;
+	DataUnit input;
 	cout << "Hello, world!";
 	return 0;
 }
